414407258aea2590ce77c556eecd607c6106d235
SageblattN <s2931713@gmail.com>
2022-02-18 18:53:23 +0300

    Mesh_lab added

---
+++ Mesh_lab/step_2.py
@@ -0,0 +1,48 @@
+import gmsh
+import math
+import os
+import sys
+
+stl_name = 'CY'
+
+gmsh.initialize()
+
+path = os.path.dirname(os.path.abspath(__file__))
+gmsh.merge(os.path.join(path, stl_name + '.stl'))
+
+
+angle = 10
+
+gmsh.option.setNumber("General.Verbosity", 2)
+
+forceParametrizablePatches = 0
+includeBoundary = 1
+
+curveAngle = 180
+
+gmsh.model.mesh.classifySurfaces(angle * math.pi / 180., includeBoundary,
+                                 forceParametrizablePatches,
+                                 curveAngle * math.pi / 180.)
+
+gmsh.option.setNumber("General.Verbosity", 5)
+
+gmsh.model.mesh.createGeometry()
+
+
+s = gmsh.model.getEntities(2)
+l = gmsh.model.geo.addSurfaceLoop([s[i][1] for i in range(len(s))])
+gmsh.model.geo.addVolume([l])
+
+gmsh.model.geo.synchronize()
+
+f = gmsh.model.mesh.field.add("MathEval")
+gmsh.model.mesh.field.setString(f, "F", "3")
+gmsh.model.mesh.field.setAsBackgroundMesh(f)
+
+gmsh.model.mesh.generate(3)
+gmsh.write('CY.msh')
+
+if '-nopopup' not in sys.argv:
+    gmsh.fltk.run()
+
+gmsh.finalize()
